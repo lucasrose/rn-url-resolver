f893037d12040efcc665714ed061dee6e89b704c
Lucas Rose <lucas@dinewise.com>
2024-12-10 16:46:30 -0500

    fix: expo-modules-core

--- src/RnUrlResolver.types.ts
+++ src/RnUrlResolver.types.ts
@@ -1,28 +1,3 @@
-/**
- * Type definition for the RnUrlResolver module.
- * This module provides functionality to resolve a URL by following redirects.
- *
- * @typedef {Object} RnUrlResolverType
- *
- * @property {Function} resolveUrl - Resolves a URL by following its redirects.
- *
- * @param {string} url - The URL to resolve.
- * @param {string} [token] - Optional authorization token to be included in the request headers.
- *
- * @returns {Promise<string>} A promise that resolves with the final resolved URL as a string.
- *
- * @example
- * const url = "https://example.com/redirect";
- * const token = "your-auth-token";
- *
- * resolveUrl(url, token)
- *   .then(resolvedUrl => {
- *     console.log(resolvedUrl); // The final resolved URL after redirects
- *   })
- *   .catch(error => {
- *     console.error(error); // Handles any errors that occur during resolution
- *   });
- */
-export type RnUrlResolverType = {
+export type RnUrlResolver = {
   resolveUrl(url: string, token?: string): Promise<string>;
 };
--- src/RnUrlResolverModule.ts
+++ src/RnUrlResolverModule.ts
@@ -1,14 +1,7 @@
-import { NativeModule, requireNativeModule } from "expo";
-import { RnUrlResolverType } from "./RnUrlResolver.types";
+import { requireNativeModule } from "expo-modules-core";
+import { RnUrlResolver } from "./RnUrlResolver.types";
 
-declare class RnUrlResolverModule extends NativeModule<RnUrlResolverType> {
-  resolveUrl(url: string, token?: string): Promise<string>;
-}
 // This call loads the native module object from the JSI.
-const RnResolver = requireNativeModule<RnUrlResolverModule>("RnUrlResolver");
+const NativeModule = requireNativeModule<RnUrlResolver>("RnUrlResolver");
 
-export default {
-  resolveUrl: async (url: string, token?: string): Promise<string> => {
-    return await RnResolver.resolveUrl(url, token);
-  },
-} as RnUrlResolverType;
+export default NativeModule;

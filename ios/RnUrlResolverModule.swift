7fe34e63062c91d3a569efaed73a5b3bd632216e
Lucas Rose <lucas@dinewise.com>
2024-12-10 23:35:31 -0500

    fix: ios redirects & bump version to 0.1.3

--- ios/RnUrlResolverModule.swift
+++ ios/RnUrlResolverModule.swift
@@ -36,23 +36,50 @@ func resolveUrl(encodedUrl: String, token: String? = nil, promise: Promise) {
     if let authToken = token {
         request.addValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
     }
-    
-    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-        if let error = error {
-            DispatchQueue.main.async {
-                promise.reject("0", "Error during request: \(error.localizedDescription)")
-            }
-            return
-        }
-        
-        DispatchQueue.main.async {
-            guard let resolvedURL = response?.url else {
-                promise.reject("0", "Unable to handle URL: Not a valid URL")
-                return
-            }
-            promise.resolve(resolvedURL.absoluteString)
+
+    let redirectManager = RedirectManager()
+    redirectManager.getFinalURL(fromURL: url) { finalURL in
+        if let finalURL = finalURL {
+            promise.resolve(finalURL.absoluteString)
+        } else {
+            promise.reject("0", "Failed to resolve final URL")
         }
     }
-    task.resume()
 }
 
+class RedirectManager: NSObject, URLSessionTaskDelegate {
+    private var finalURL: URL?
+    private var completion: ((URL?) -> Void)?
+    func getFinalURL(fromURL initialURL: URL, completion: @escaping (URL?) -> Void) {
+        self.completion = completion
+
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        
+        let task = session.dataTask(with: initialURL) { _, _, error in
+            if let error = error {
+                print("Error resolving URL: \(error)")
+                completion(nil)
+            } else {
+                completion(self.finalURL ?? initialURL)
+            }
+        }
+        task.resume()
+    }
+    
+    // URLSessionTaskDelegate method to handle redirection
+    func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
+        
+        // Here, 'request.url' is the new URL the task is being redirected to.
+        // We call the completion handler with nil to prevent the redirection from happening
+        // because we just want to capture the URL.
+        completionHandler(nil)
+        
+        // We assume the delegate method will be called for the final redirection.
+        // This may need refinement to handle multiple redirections if necessary.
+        if let finalURL = request.url {
+            self.finalURL = request.url
+            // Do something with the final URL
+            print("Final URL is: \(finalURL)")
+        }
+    }
+}
